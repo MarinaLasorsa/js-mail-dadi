f2b86bd350333eff633f1053772f56895c2c7363
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-07 15:53:45 +0100

    ADD: steps in comments to check the user's email validity from a list and print result in html

---
+++ Mail/JS/app.js
@@ -0,0 +1,39 @@
+/*ESERCIZIO: Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+
+//1. Creare input mail e bottone submit per ricevere i dati dall'utente. 
+//Creare div dove inserire messaggio sull'esito del controllo.
+
+//2. Recuperare dal DOM input mail con getElementById
+
+//3. Dichiarare in variabile il valore di input mail
+
+//4. Creare un array di varie mail che possono accedere
+
+//5. Recuperare dal dom l'elemento bottone con querySelector
+
+//6. Creare un eventListener per il click dell'elemento bottone
+
+//7. Creare variabile di controllo "la mail è nell'array?" = false (fino a prova contraria non c'è)
+
+//8. Creare ciclo per controllare ogni mail nell'array
+
+//9. Creare variabile per elemento attuale dell'array (nomeArray[i])
+
+//10. SE l'elemento dell'array è uguale === al valore dell'input mail
+    //ALLORA variabile di controllo "la mail è nell'array?" = true (in questo caso c'è)
+
+//11. Chiudere ciclo
+
+//12. Recuperare dal dom l'elemento div dove inserire messaggio di esito
+
+//13. SE variabile di controllo = true
+    //ALLORA inserisco nell'innerHTML dell'elemento div "Congratulazioni! Puoi accedere al portale."
+
+    //ALTRIMENTI inserisco nell'innerHTML dell'elemento div "Ci dispiace, non hai l'autorizzazione ad accedere al portale."
+
+
+
+
