feab842bcb2996c9e89d7fbb8b17c25f0ea2fc74
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-07 16:51:08 +0100

    ADD: code to check user email validity from list and print results in HTML

--- Mail/JS/app.js
+++ Mail/JS/app.js
@@ -7,32 +7,62 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 //Creare div dove inserire messaggio sull'esito del controllo.
 
 //2. Recuperare dal DOM input mail con getElementById
+const inputEmailElement = document.getElementById("email");
+//console.log(inputEmailElement);
 
-//3. Dichiarare in variabile il valore di input mail
+//3. Creare un array di varie mail che possono accedere
+const emailsList = ["marina@gmail.com", "alberto@gmail.com", "donato@gmail.com", "samuel@gmail.com", "gianluca@gmail.com"];
+//console.log(emailsList);
 
-//4. Creare un array di varie mail che possono accedere
+//4. Recuperare dal dom l'elemento bottone con querySelector
+const ButtonElement = document.querySelector('[type="submit"]');
+//console.log(ButtonElement);
 
-//5. Recuperare dal dom l'elemento bottone con querySelector
+//5. Creare un eventListener per il click dell'elemento bottone
+ButtonElement.addEventListener('click', function() {
 
-//6. Creare un eventListener per il click dell'elemento bottone
+    //6. Dichiarare in variabile il valore di input mail
+    let inputEmailValue = inputEmailElement.value;
 
-//7. Creare variabile di controllo "la mail è nell'array?" = false (fino a prova contraria non c'è)
+    //7. Creare variabile di controllo "la mail è nell'array?" = false (fino a prova contraria non c'è)
+    let isEmailInArray = false;
 
-//8. Creare ciclo per controllare ogni mail nell'array
+    //8. Creare ciclo per controllare ogni mail nell'array
+    for (let i = 0; i < emailsList.length; i++) {
 
-//9. Creare variabile per elemento attuale dell'array (nomeArray[i])
+        //9. Creare variabile per elemento attuale dell'array (nomeArray[i])
+        const email = emailsList[i];
 
-//10. SE l'elemento dell'array è uguale === al valore dell'input mail
-    //ALLORA variabile di controllo "la mail è nell'array?" = true (in questo caso c'è)
+        //10. SE l'elemento dell'array è uguale === al valore dell'input mail
+        //ALLORA variabile di controllo "la mail è nell'array?" = true (in questo caso c'è)
+        if (email === inputEmailValue) {
+            isEmailInArray = true;
+        }
+        //11. Chiudere ciclo
+    }
 
-//11. Chiudere ciclo
+    //12. Recuperare dal dom l'elemento div dove inserire messaggio di esito
+    const divResultMessageElement = document.querySelector(".result-message");
+    //console.log(divResultMessageElement);
 
-//12. Recuperare dal dom l'elemento div dove inserire messaggio di esito
-
-//13. SE variabile di controllo = true
+    //13. SE variabile di controllo = true
     //ALLORA inserisco nell'innerHTML dell'elemento div "Congratulazioni! Puoi accedere al portale."
-
+    if (isEmailInArray === true) {
+        divResultMessageElement.innerHTML = "Congratulazioni! Puoi accedere al portale.";
+    } 
+    
     //ALTRIMENTI inserisco nell'innerHTML dell'elemento div "Ci dispiace, non hai l'autorizzazione ad accedere al portale."
+    else {
+        divResultMessageElement.innerHTML = "Ci dispiace, non hai l'autorizzazione ad accedere al portale.";
+    }
+
+})
+
+
+
+
+
+    
 
 
 
