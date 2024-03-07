fe71d580ddac5166756db25697108553acdc9dd8
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-07 17:47:17 +0100

    ADD: HTML skeleton and basic style + steps in comments for dice game where a random number is generated and added in html for both user and computer, and game results are added in html depending on which number is higher.

---
+++ Gioco dei dadi/JS/app.js
@@ -0,0 +1,32 @@
+/*ESERCIZIO: Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+
+//Recuperare il bottone dal DOM tramite querySelector
+
+//Recuperare elemento div user-result tramite querySelector
+
+//Recuperare elemento div computer-result tramite querySelector
+
+//Recuperare elemento div game-result dal DOM tramite querySelector
+
+//Creare un eventListener per il click del bottone
+
+//Dichiarare variabile numero random utente da 1 a 6
+
+//Inserire numero random in elemento div user-result tramite innerHTML
+
+//Dichiarare variabile numero random computer da 1 a 6
+
+//Inserire numero random computer in elemento div tramite innerHTML
+
+//SE numero random utente è maggiore di numero random computer,
+//ALLORA 
+//Inserire in div game-result "HAI VINTO!" tramite innerHTML
+
+//ALTRIMENTI SE numero random utente è uguale a numero random computer,
+//ALLORA
+//Inserire in div game-result "HAI PAREGGIATO!" tramite innerHTML
+
+//ALTRIMENTI
+//Inserire in div game-result "HAI PERSO!" tramite innerHTML
